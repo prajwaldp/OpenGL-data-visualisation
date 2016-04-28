982c4492a873f80ddea1282cdab148754cd45565
Prajwal Prakash <prajwalpy6@gmail.com>
2016-04-28 06:44:43 +0530

    add gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,29 @@
 
 using namespace std;
 
-const int WINDOW_WIDTH = 640*2;
-const int WINDOW_HEIGHT = 480;
+const int WINDOW_WIDTH = 500;
+const int WINDOW_HEIGHT = 500;
 
 typedef struct {
   GLfloat x, y, z;
   GLfloat r, g, b, a;
 } Vertex;
 
-typedef struct
-{
+typedef struct {
   GLfloat x, y, z;
 } Data;
+
+typedef struct {
+  GLfloat r, g, b, a;
+} Color;
+
+typedef vector<Data> Dataset;
 
 namespace Primitive {
   void point(Vertex, GLfloat);
   void line(Vertex, Vertex, GLfloat);
+  void unfilled_quad(Vertex, Vertex, Vertex, Vertex, GLfloat);
+  void filled_quad(Vertex, Vertex, Vertex, Vertex);
 };
 
 void Primitive::point(Vertex v, GLfloat size) {
@@ -40,23 +47,44 @@ void Primitive::line(Vertex v1, Vertex v2, GLfloat width) {
   glEnd();
 }
 
-/*
+void Primitive::unfilled_quad(Vertex v1, Vertex v2, Vertex v3, Vertex v4, GLfloat width) {
+  Primitive::line(v1, v2, width);
+  Primitive::line(v2, v3, width);
+  Primitive::line(v3, v4, width);
+  Primitive::line(v4, v1, width);
+}
+
+void Primitive::filled_quad(Vertex v1, Vertex v2, Vertex v3, Vertex v4) {
+  glBegin(GL_POLYGON);
+  glColor4f(v1.r, v1.g, v1.b, v1.a);
+  glVertex3f(v1.x, v1.y, v1.z);
+
+  glColor4f(v2.r, v2.g, v2.b, v2.a);
+  glVertex3f(v2.x, v2.y, v2.z);
+
+  glColor4f(v3.r, v3.g, v3.b, v3.a);
+  glVertex3f(v3.x, v3.y, v3.z);
+
+  glColor4f(v4.r, v4.g, v4.b, v4.a);
+  glVertex3f(v4.x, v4.y, v4.z);
+  glEnd();
+}
+
 class DataSetReader
 {
 public:
-  dataset read();
+  Dataset read();
 };
 
-dataset DataSetReader::read() {
+Dataset DataSetReader::read() {
   int i;
 
-  example item;
-  dataset tester;
+  Data item;
+  Dataset tester;
 
-  for (i = 1; i <= 5; i++) {
+  for (i = 1; i <= 10; i++) {
     item.x = i;
     item.y = i;
-    item.label = "New";
 
     tester.push_back(item);
   }
@@ -73,17 +101,18 @@ public:
 void BarGraph::display()
 {
   int i;
+  int max_x, max_y, min_x, min_y;
+  float width;
 
-  int max_x, max_y, win_x, win_y;
-  int start_x, start_y, end_x, end_y;
-
-  dataset X;
+  Dataset X;
 
   DataSetReader reader;
   X = reader.read();
 
   max_x = 0;
   max_y = 0;
+  min_x = 0;
+  min_y = 0;
 
   for (i = 0; i < X.size(); i++) {
     if (max_x < X[i].x) {
@@ -93,67 +122,46 @@ void BarGraph::display()
     if (max_y < X[i].y) {
       max_y = X[i].y;
     }
+
+    if (min_x > X[i].x) {
+      min_x = X[i].x;
+    }
+
+    if (min_y > X[i].y) {
+      min_y = X[i].y;
+    }
   }
 
-  win_x = max_x + 3;
-  win_y = max_y + 3;
+  Color border_color = { 77 / 255.0, 83 / 255.0, 89 / 255.0, 1 };
 
-  gluOrtho2D(0, win_x, 0, win_y);
+  Vertex v1 = { -0.8f, -0.8f, 0.f, border_color.r, border_color.g, border_color.b, border_color.a};
+  Vertex v2 = { -0.8f, 0.8f, 0.f, border_color.r, border_color.g, border_color.b, border_color.a};
+  Vertex v3 = { 0.8f, 0.8f, 0.f, border_color.r, border_color.g, border_color.b, border_color.a};
+  Vertex v4 = { 0.8f, -0.8f, 0.f, border_color.r, border_color.g, border_color.b, border_color.a};
+  Primitive::unfilled_quad(v1, v2, v3, v4, 1);
 
-  start_x = 1;
-  start_y = 1;
+  width = 1.6 / X.size();
+  Color graph_color = { 23 / 255.0, 55 / 255.0, 83 / 255.0, 1 };
 
-  end_x = win_x - 2;
-  end_y = win_y - 2;
-
-  glColor3f(109.0 / 255, 174.0 / 255, 219.0 / 255);
-
-  // drawing x axis
-
-  glBegin(GL_LINES);
-  glVertex2f(start_x, start_y);
-  glVertex2f(end_x + 1, start_y);
-
-  glVertex2f(end_x + 1, start_y);
-  glVertex2f(end_x + 1, end_y + 1);
-
-  glVertex2f(end_x + 1, end_y + 1);
-  glVertex2f(start_x, end_y + 1);
-
-  glVertex2f(start_x, start_y);
-  glVertex2f(start_x, end_y + 1);
-
-  glEnd();
-
-  // draw graph lines
-
-  glColor3f(219.0 / 255, 214.0 / 255, 214.0 / 255);
-
-  for (i = 1; i <= X.size(); i++) {
-    glBegin(GL_LINES);
-    glVertex2f(start_x, start_y + i);
-    glVertex2f(end_x + 1, start_y + i);
-    glEnd();
-  }
-
-  // plotting histogram
-
-  glLineWidth(250.0 / X.size());
-  glColor3f(23.0 / 255, 55.0 / 255, 83.0 / 255);
+  float current = -0.8;
+  float height;
   for (i = 0; i < X.size(); i++) {
-    glBegin(GL_LINES);
-    glVertex3f(X[i].x + start_x, start_y, 1);
-    glVertex3f(X[i].x + start_x, X[i].y + start_y, 1);
-    glEnd();
+    height = -0.8 + 1.6 * X[i].y / (max_y - min_y);
+    Vertex v1 = {current + 0.02, -0.8, 0, graph_color.r, graph_color.g, graph_color.b, graph_color.a};
+    Vertex v2 = {current + width - 0.04, -0.8, 0, graph_color.r, graph_color.g, graph_color.b, graph_color.a};
+    Vertex v3 = {current + width - 0.04, height, 0, graph_color.r, graph_color.g, graph_color.b, graph_color.a};
+    Vertex v4 = {current + 0.02, height, 0, graph_color.r, graph_color.g, graph_color.b, graph_color.a};
+    Primitive::filled_quad(v1, v2, v3, v4);
+    current += width;
   }
 
 }
-*/
 
 void display()
 {
-  Vertex v1 = {0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 1.f};
-  Vertex v2 = {1.f, 0.f, 0.f, 0.f, 0.f, 0.f, 1.f};
+  BarGraph bar_graph;
+  bar_graph.display();
+
   glutSwapBuffers();
 
   glFlush();
@@ -167,9 +175,6 @@ void reshape(int w, int h)
 
 void init()
 {
-  float ratio;
-
-  ratio = (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT;
   glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   glClearColor(238.0 / 255, 236.0 / 255, 238.0 / 255, 1);
 
@@ -177,7 +182,7 @@ void init()
   glLoadIdentity();
 
   //Orthographic Projection
-  glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+  glOrtho(-1.f, -1.f, -1.f, 1.f, 1.f, -1.f);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
