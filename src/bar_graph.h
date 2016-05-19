b7abb7d7afae10fa2130f56146f6493f8fdd85af
Prajwal Prakash <prajwalpy6@gmail.com>
2016-05-19 08:01:47 +0530

    flexible data set reader

--- src/bar_graph.h
+++ src/bar_graph.h
@@ -1,6 +1,8 @@
 #ifndef BAR_GRAPH_H
 #define BAR_GRAPH_H
 
+#include <math.h>
+
 class BarGraph
 {
 public:
@@ -15,31 +17,23 @@ void BarGraph::display()
 
   Dataset X;
 
-  DataSetReader reader;
+  DataSetReader reader(1);
   X = reader.read();
 
   max_x = 0;
-  max_y = 0;
   min_x = 0;
-  min_y = 0;
 
   for (i = 0; i < X.size(); i++) {
     if (max_x < X[i].x) {
       max_x = X[i].x;
     }
 
-    if (max_y < X[i].y) {
-      max_y = X[i].y;
-    }
-
     if (min_x > X[i].x) {
       min_x = X[i].x;
     }
-
-    if (min_y > X[i].y) {
-      min_y = X[i].y;
-    }
   }
+
+  max_x += 1;
 
   Color border_color = { 77 / 255.0, 83 / 255.0, 89 / 255.0, 1 };
 
@@ -50,7 +44,7 @@ void BarGraph::display()
   Primitive::unfilled_quad(v1, v2, v3, v4, 1);
 
   width = 1.6 / X.size();
-  gap = width / 100;
+  gap = width / 50;
   Color graph_color = { 23 / 255.0, 55 / 255.0, 83 / 255.0, 1 };
   Color graph_lines_color = { 210 / 255.0, 210 / 255.0, 210 / 255.0, 1 };
 
@@ -64,14 +58,14 @@ void BarGraph::display()
     Vertex v2 = {0.8, height, 0, graph_lines_color.r, graph_lines_color.g, graph_lines_color.b, graph_lines_color.a};
     Primitive::line(v1, v2, 1);
     ostringstream ss;
-    ss << i * (max_y) / 10;
+    ss << i * (max_x) / 10;
     std::string s(ss.str());
     Primitive::write(s, -0.9, height);
   }
 
   // Plotting bar graph
   for (i = 0; i < X.size(); i++) {
-    height = -0.8 + 1.6 * X[i].y / (max_y - min_y);
+    height = -0.8 + 1.6 * X[i].x / ceil(max_x - min_x);
     Vertex v1 = {current + gap, -0.8, 0, graph_color.r, graph_color.g, graph_color.b, graph_color.a};
     Vertex v2 = {current + width - 2 * gap, -0.8, 0, graph_color.r, graph_color.g, graph_color.b, graph_color.a};
     Vertex v3 = {current + width - 2 * gap, height, 0, graph_color.r, graph_color.g, graph_color.b, graph_color.a};
--- src/data_set_reader.h
+++ src/data_set_reader.h
@@ -3,25 +3,47 @@
 
 #include "../lib/csv.h"
 
+using namespace io;
+
 class DataSetReader
 {
+  int degree;
 public:
+  DataSetReader(int);
   Dataset read();
 };
 
+DataSetReader::DataSetReader(int degree=3): degree(degree) {}
+
 Dataset DataSetReader::read() {
   int i;
-  Data item;
-  Dataset tester;
+  Data item1, item2, item3;
+  Dataset tester1, tester2, tester3;
 
-  io::CSVReader<4> in("data.json");
-  in.read_header(io::ignore_extra_column, "label", "x", "y", "z");
-  std::string label; GLfloat x, y, z;
-  while(in.read_row(item.label, item.x, item.y, item.z)){
-    tester.push_back(item);
+  CSVReader<2> in1("data.json");
+  CSVReader<3> in2("data.json");
+  CSVReader<4> in3("data.json");
+
+  cout << "Reading data\n" << "Degree = " << degree << endl;
+
+  in1.read_header(ignore_extra_column, "label", "x");
+  while(in1.read_row(item1.label, item1.x)){
+    tester1.push_back(item1);
   }
 
-  return tester;
+  in3.read_header(ignore_extra_column, "label", "x", "y", "z");
+  while(in3.read_row(item3.label, item3.x, item3.y, item3.z)){
+    tester3.push_back(item3);
+  }
+
+  if (degree == 1) {
+    return tester1;
+  } else if (degree == 2) {
+    return tester2;
+  } else if (degree == 3) {
+    return tester3;
+  }
+
 }
 
 #endif
